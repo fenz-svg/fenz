18f33dfbbd5314f0bf2d330849a05e0faac6fb0d
fenz-svg <fenzsupport@gmail.com>
2026-07-01 13:04:10 -0300

    Fix base path for custom domain fenzweb.com

--- fenz-react/vite.config.js
+++ fenz-react/vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   // base apunta a tu repo de GitHub Pages: https://fenz-svg.github.io/fenz/
-  base: "/fenz/",
+    base: "/",
 });
